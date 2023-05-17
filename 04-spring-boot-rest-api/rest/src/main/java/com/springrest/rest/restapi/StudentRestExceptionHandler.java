8f645e497ceab062be34211319a5f890261ef6fa
eda <edatan4@gmail.com>
2023-05-17 10:31:58 +0300

    -Exception Handling with use @ExceptionHandler anatation.

---
+++ 04-spring-boot-rest-api/rest/src/main/java/com/springrest/rest/restapi/StudentRestExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.springrest.rest.restapi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class StudentRestExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setMessage(exc.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    //For handling any type of exception.
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handle(Exception e){
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setMessage(e.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+
+}
